60dc9438d74adebd02ade2372d8c00867a32895f
Lucas dos Anjos <lucas.anjos@bizsys.com.br>
2024-01-24 17:13:17 -0300

    modificação no backend connection DB para usar variaveis de ambiente

--- pratica/backend/index.php
+++ pratica/backend/index.php
@@ -3,7 +3,7 @@ header('Access-Control-Allow-Origin: *');
 $host = 'database-1.crocuoiuc11a.us-east-2.rds.amazonaws.com';
 $db = 'testeDB';
 $user = 'admin';
-$pass = 'QMxQP2BjATmfb3skNAXs';
+$pass = $_ENV['DB_PASS'];
 
 $con_sqli = new mysqli($host, $user, $pass, $db); // Comando para conectar com BD MySQL
 
